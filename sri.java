d2ae1cbf544a34bd5462e6b37c5562caadd5637a
srivamsidandu <dsv9121535759@gmail.com>
2022-02-11 12:14:50 +0530

    added linked list code

---
+++ sri.java
@@ -0,0 +1,58 @@
+package com.SriVamsi;
+public class sri {
+    public  class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    Node head = null;
+    Node tail = null;
+
+    public void addnode(int data){
+        Node newnode = new Node(data);
+        if(head==null){
+            head= newnode;
+        }
+        else{
+            tail.next=newnode;
+        }
+        tail=newnode;
+    }
+    public void reverse(Node current){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        else{
+            if(current.next==null){
+                System.out.print(current.data+" ");
+                return;
+            }
+            reverse(current.next);
+            System.out.print(current.data+" ");
+        }
+    }
+    public void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp= temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        sri ncx = new sri();
+        ncx.addnode(23);
+        ncx.addnode(34);
+        ncx.addnode(45);
+        ncx.display();
+
+        System.out.println("reverse");
+        ncx.reverse(ncx.head);
+    }
+
+}
